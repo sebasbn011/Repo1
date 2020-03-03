3676b2d11dfcc31fce3d38727be5550f68630b31
sebasbn011 <55513782+sebasbn011@users.noreply.github.com>
2020-03-03 15:11:53 -0500

    cambios en dev en el remoto
    cambios en dev en el remoto

--- example/src/main/java/poo/example/SmartPhone.java
+++ example/src/main/java/poo/example/SmartPhone.java
@@ -10,18 +10,18 @@ public class SmartPhone extends Celular{
 	public SmartPhone(String marca, String modelo, String color, float pixelesCamara, float tamanioMemoriaRam,
 			float tamaoDisco) {
 		super(marca, modelo, color);
-		///this.pixelesCamara = pixelesCamara;
-		///this.tamanioMemoriaInterna = tamanioMemoriaRam;
-		///this.tamanioMemoriaExterna = tamaoDisco;
+		this.pixelesCamara = pixelesCamara;
+		this.tamanioMemoriaInterna = tamanioMemoriaRam;
+		this.tamanioMemoriaExterna = tamaoDisco;
 	}
 	// método sobrescrito (override), utilizo código de la clase Celular y añado código que necesito 
 	@Override
 	public void informarCaracteristicas() {
 		// TODO Auto-generated method stub
 		super.informarCaracteristicas();
-		//System.out.println(String.format("SmartPhone calidad cámara: %s pixeles", pixelesCamara));
-		//System.out.println(String.format("SmartPhone tamaño memoria interna: %s GB", tamanioMemoriaInterna));
-		//System.out.println(String.format("SmartPhone tamaño memoria externa: %s GB", tamanioMemoriaExterna));
+		System.out.println(String.format("SmartPhone calidad cámara: %s pixeles", pixelesCamara));
+		System.out.println(String.format("SmartPhone tamaño memoria interna: %s GB", tamanioMemoriaInterna));
+		System.out.println(String.format("SmartPhone tamaño memoria externa: %s GB", tamanioMemoriaExterna));
 	}
 	
 }
